0101c71d3bd9d430c90f530f5bbf41d7ad8e68a5
yuvlian <138542238+yuvlian@users.noreply.github.com>
2024-11-08 21:40:26 +0700

    chore: add placeholder for page builder

---
+++ page/__main__.py
@@ -0,0 +1,24 @@
+from page.build import (
+    index,
+    announcer_index,
+    ego_index,
+    item_index,
+    personality_index,
+)
+from page.build import (
+    announcers,
+    egos,
+    items,
+    personalities,
+)
+
+if __name__ == "__main__":
+    index.build()
+    announcer_index.build()
+    ego_index.build()
+    item_index.build()
+    personality_index.build()
+    announcers.build()
+    egos.build()
+    items.build()
+    personalities.build()
